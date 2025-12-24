31e6ca2fa4a85e5fc1077b39fdf9403622d4ab01
Reazul-Islam02 <reaz4524@gmail.com>
2025-12-24 22:54:44 +0600

    firstt

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5175',
-        'https://travel-easy-anm.netlify.app/',
+        'https://travel-easy-anm.netlify.app',
         'https://travelease-client.firebaseapp.com'
     ],
     credentials: true
